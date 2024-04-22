22fdfb1960f8e3d97cc6e77d9c61dbb51eb95c9a
Nils Steinmann <143550121+NilsSteinmann@users.noreply.github.com>
2024-04-22 10:36:40 -0600

    Update Final.c

--- Final.c
+++ Final.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct
 {
@@ -63,6 +64,11 @@ Ficha* crearFichas(int numFichas)
 			}
 		}
 	}
+	fichas[104].numero = 0;
+	fichas[104].color = 'C';
+	fichas[105].numero = 0;
+	fichas[105].color = 'C';
+	
 	return fichas;
 }
 
@@ -70,7 +76,7 @@ Ficha* crearFichas(int numFichas)
 struct Stack {
     int top;
     unsigned capacity;
-    int* array;
+    Ficha* array;
 };
 
 // function to create a stack of given capacity. It initializes size of
@@ -80,7 +86,7 @@ struct Stack* createStack()
     struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
     stack->capacity = 110;
     stack->top = -1;
-    stack->array = (int*)malloc(stack->capacity * sizeof(int));
+    stack->array = (Ficha*)malloc(stack->capacity * sizeof(Ficha));
     return stack;
 }
 
@@ -97,7 +103,7 @@ int isEmpty(struct Stack* stack)
 }
 
 // Function to add an item to stack.  It increases top by 1
-void push(struct Stack* stack, int item)
+void push(struct Stack* stack, Ficha item)
 {
     if (isFull(stack)){
       printf("\tOverflow!\n");
@@ -105,43 +111,68 @@ void push(struct Stack* stack, int item)
     }
 
     stack->array[++stack->top] = item;
-    printf("%d pushed to stack\n", item);
+    printf("(%d%c) pushed to stack\n", item.numero, item.color);
 }
 
 // Function to remove an item from stack.  It decreases top by 1
-int pop(struct Stack* stack)
+Ficha pop(struct Stack* stack)
 {
     if (isEmpty(stack)){
       printf("\tUnderflow!\n");
-      return INT_MIN;
+      Ficha emptyFicha = { .color = '\0', .numero = INT_MIN };
+      return emptyFicha;
     }
     return stack->array[stack->top--];
 }
 
 // Function to return the top from stack without removing it
-int peek(struct Stack* stack)
+Ficha peek(struct Stack* stack)
 {
     if (isEmpty(stack)){
       printf("\tUnderflow!\n");
-      return INT_MIN;
+	  Ficha emptyFicha = { .color = '\0', .numero = INT_MIN };
+      return emptyFicha;;
     }
 
     return stack->array[stack->top];
 }
 
-//int revolver(Ficha* fichasPartida)
-//{
-	
-	//Stack* Pozo = createStack();
-	
-//}
 
+int revolver(Ficha* fichasPartida)
+{
+    // Crear una stack para las fichas
+    struct Stack* Pozo = createStack();
+
+    // Arreglo de índices para representar las posiciones de las fichas
+    int* indices = (int*)malloc(sizeof(int) * 106);
+    for (int i = 0; i < 106; ++i)
+        indices[i] = i;
+
+    // Barajar los índices aleatoriamente
+    srand(time(NULL));
+    for (int i = 106 - 1; i > 0; --i) {
+        int j = rand() % (i + 1);
+        int temp = indices[i];
+        indices[i] = indices[j];
+        indices[j] = temp;
+    }
+
+    // Iterar sobre los índices barajados y agregar las fichas correspondientes a la stack
+    for (int i = 0; i < 106; ++i) {
+        Ficha ficha = fichasPartida[indices[i]];
+        push(Pozo, ficha);
+        }
+	free(indices);
+    return 0;
+}
 
 int main()
 {
 	int elec, i=0;
-	int numFichas = 104;
+	int numFichas = 106;
 	Ficha* fichasPartida = crearFichas(numFichas);
+	while(1){
+	system("CLS");
 	system("color 0E");
 	printf( "BIENVENDIO A RUMMY DE LOS EDAMIGOS!\n" );
 	printf( "Elige una opcion:\n1.-JUGAR\n2.-VER\n3.-SALIR\n" );
@@ -162,10 +193,13 @@ int main()
         	printf(".");
         	sleep(1);
         	printf(".");
-        	//revolver(fichasPartida);
+        	revolver(fichasPartida);
+        	system("pause");
+        	//repartir(Pozo);
 			break;
 		case 2:
 			printf("Construcion too...");
+			system("pause");
 			break;
 		case 3:
 			return;
@@ -174,6 +208,7 @@ int main()
 			sleep(3);
 			system("CLS");
 			main();
+	}
 	}
 	return 0;
 }
